e4958e23918cac8d54a9a28a70825d33bdbfdc3a
Kasper Holbek Jensen <khj@autobutler.dk>
2014-10-18 13:43:19 +0200

    refac and add first version of encoding

--- lib/bencoder.rb
+++ lib/bencoder.rb
@@ -1,18 +1,124 @@
+require 'stringio'
 class BEncoder
   def self.encode(object)
     case object
     when Symbol
       encode object.to_s
     when String
-      "#{object.length}:#{object}"
+      encode_string object
     when Integer
-      "i#{object}e"
+      encode_int object
     when Array
-      encoded_content = object.inject("l") { |result, el| result += encode(el) }
-      encoded_content += "e"
+      encode_array object
     when Hash
-      encoded = object.inject("d") { |result, (k,v)| result += "#{encode(k.to_s)}#{encode(v)}" }
-      encoded += "e"
+      encode_hash object
     end
   end
+
+  def self.decode(string)    
+    parse string
+  end
+
+
+
+  private
+
+  def self.parse(string)
+    case string[0]
+    when 'i'
+      parse_int string
+    when 'l'
+      parse_list string
+    when 'd'
+      parse_dict string
+    else
+      parse_string string
+    end
+  end
+
+  def self.parse_list(string)
+    if string.is_a? StringIO
+      str = string
+      str.getc if peek(str) == 'l'
+    elsif string[0] == 'l' && string[-1] == 'e'
+      str = StringIO.new string[1..-2]
+    else 
+      raise 'Not a list'
+    end
+    list = []
+    until peek(str) == 'e' || str.eof?
+      case peek(str)
+      when 'i'
+        list << parse_int(str.gets sep='e')
+      when 'l'
+        list << parse_list(str)
+      when 'd'
+        list << parse_dict(str)
+      when ->(e) { e =~ /\d/ }
+        length = str.gets(sep=':').to_i
+        list << str.gets(length)
+      else
+        raise str.read
+      end
+    end
+    str.getc
+    list
+  end
+
+  def self.parse_dict(string)
+    if string.is_a? StringIO
+      string.getc if peek(string) == 'd'
+      list_of_keys_and_values = parse_list(string)
+    elsif string[0] == 'd' && string[-1] == 'e'
+      list_of_keys_and_values = parse_list("l#{string[1..-2]}e")
+    else
+      raise 'Something in hash is wrong!'
+    end
+    hash = {}
+    list_of_keys_and_values.each_slice(2) do |k,v|
+      hash[k] = v
+    end
+    hash
+  end
+
+  def self.parse_int(string)
+    if string[0] == 'i' && string[-1] == 'e'
+      string[1..-2].to_i
+    else
+      raise 'Int is formatted wrong'
+    end
+  end
+
+  def self.parse_string(string)
+    length, content = string.split ':'
+    if content.length == length.to_i
+      content
+    else
+      raise 'String length is messed up'
+    end
+  end
+
+  def self.encode_string(string)
+    "#{string.length}:#{string}"
+  end
+
+  def self.encode_int(int)
+    "i#{int}e"
+  end
+
+  def self.encode_array(array)
+    array.inject("l") { |result, el| result += encode(el) } + "e"
+  end
+
+  def self.encode_hash(hash)
+    hash.inject("d") { |result, (k,v)| result += "#{encode(k.to_s)}#{encode(v)}" } + 'e'
+  end
+
+  def self.peek(io)
+    char = io.getc
+    io.ungetc char
+    puts char
+    char
+  end
+
 end
--- test/test_bencoder.rb
+++ test/test_bencoder.rb
@@ -1,10 +1,13 @@
 require 'minitest/autorun'
 require 'bencoder'
 
-class TestBencoder < Minitest::Unit::TestCase
+class TestBencoder < Minitest::Test
   def setup
     @be = BEncoder
   end
+
+  #encoding
+
   def test_string_encoding
     assert_equal '7:Someday', @be.encode('Someday')
   end
@@ -21,7 +24,29 @@ class TestBencoder < Minitest::Unit::TestCase
     assert_equal 'd4:what2:is2:up4:dawge', @be.encode({what: 'is', up: 'dawg'})
   end
 
-  def test_nested_values
+  def test_nested_encoding
     assert_equal 'ld4:somel5:times3:youe5:gotta3:let2:go1:!ei2e5:wooooli1e1:21:3ee', @be.encode([{'some' => ['times', 'you'], gotta: 'let', go: '!'}, 2, "woooo", [1, '2', "3"]])
+  end
+
+  #decoding
+
+  def test_string_decoding
+    assert_equal "I'm free!", @be.decode("9:I'm free!")
+  end
+
+  def test_int_decoding
+    assert_equal 321, @be.decode('i321e')
+  end
+
+  def test_array_decoding
+    assert_equal ['1', 2, 3], @be.decode('l1:1i2ei3ee')
+  end
+
+  def test_hash_decoding
+    assert_equal({'abc' => 'def', 'ghi' => 32}, @be.decode("d3:abc3:def3:ghii32ee"))
+  end
+
+  def test_nested_decoding
+    assert_equal [{'some' => ['times', 'you'], 'gotta' => 'let', 'go' => '!'}, 2, "woooo", [1, '2', "3"]], @be.decode('ld4:somel5:times3:youe5:gotta3:let2:go1:!ei2e5:wooooli1e1:21:3ee')
   end
 end
