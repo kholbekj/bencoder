06961d67307b5e39fd0fc50d1192fcc6380137d7
Emil Kampp <emil@kampp.me>
2014-10-19 10:52:18 +0200

    Split functionality into two, distinct classes
    One (Decoder) for decoding and one (Encoder) for encoding.

--- lib/bencoder.rb
+++ lib/bencoder.rb
@@ -3,136 +3,162 @@ require 'stringio'
 class InvalidEncodingError < StandardError; end
 class UnencodableTypeError < StandardError; end
 
-class BEncoder
-  class << self
-    def encode(object)
-      case object
-      when Symbol
-        encode object.to_s
-      when String
-        encode_string object
-      when Integer
-        encode_int object
-      when Array
-        encode_array object
-      when Hash
-        encode_hash object
-      else
-        raise UnencodableTypeError, "Cannot encode instance of type #{object.class}"
-      end
+class Decoder
+  attr_reader :string
+  
+  def self.parse(string)
+    new(string).parse
+  end
+  
+  def initialize(string)
+    @string = string
+  end
+  
+  def parse
+    case string[0]
+    when 'i'
+      parse_int string
+    when 'l'
+      parse_list string
+    when 'd'
+      parse_dict string
+    else
+      parse_string string
     end
+  end
 
-    def decode(string)    
-      parse string
+protected
+  
+  def peek(io)
+    char = io.getc
+    io.ungetc char
+    char
+  end
+
+  def parse_list(string)
+    if string.is_a? StringIO
+      str = string
+      str.getc if peek(str) == 'l'
+    elsif string[0] == 'l' && string[-1] == 'e'
+      str = StringIO.new string[1..-2]
+    else 
+      raise InvalidEncodingError, 'List does not have a closing e'
     end
+    parse_io_list str
+  end
 
+  def parse_dict(string)
+    if string.is_a? StringIO
+      string.getc if peek(string) == 'd'
+      list_of_keys_and_values = parse_list(string)
+    elsif string[0] == 'd' && string[-1] == 'e'
+      list_of_keys_and_values = parse_list("l#{ string[1..-2] }e")
+    else
+      raise InvalidEncodingError, 'Dict does not have a closing e'
+    end
+    make_hash_from_array list_of_keys_and_values
+  end
 
-
-    private
-
-    def parse(string)
-      case string[0]
+  def parse_io_list(io)
+    list = []
+    until peek(io) == 'e' || io.eof?
+      case peek(io)
       when 'i'
-        parse_int string
+        list << parse_int(io.gets sep='e')
       when 'l'
-        parse_list string
+        list << parse_list(io)
       when 'd'
-        parse_dict string
+        list << parse_dict(io)
+      when ->(e) { e =~ /\d/ }
+        length = io.gets(sep=':').to_i
+        list << io.gets(length)
       else
-        parse_string string
+        raise InvalidEncodingError, "Encountered unexpected identifier #{ peek io }"
       end
     end
+    io.getc
+    list
+  end
 
-    def parse_list(string)
-      if string.is_a? StringIO
-        str = string
-        str.getc if peek(str) == 'l'
-      elsif string[0] == 'l' && string[-1] == 'e'
-        str = StringIO.new string[1..-2]
-      else 
-        raise InvalidEncodingError, 'List does not have a closing e'
-      end
-      parse_io_list str
+  def parse_int(string)
+    if string[0] == 'i' && string[-1] == 'e'
+      string[1..-2].to_i
+    else
+      raise InvalidEncodingError, 'Integer does not have closing e'
     end
+  end
 
-    def parse_dict(string)
-      if string.is_a? StringIO
-        string.getc if peek(string) == 'd'
-        list_of_keys_and_values = parse_list(string)
-      elsif string[0] == 'd' && string[-1] == 'e'
-        list_of_keys_and_values = parse_list("l#{ string[1..-2] }e")
-      else
-        raise InvalidEncodingError, 'Dict does not have a closing e'
-      end
-      make_hash_from_array list_of_keys_and_values
+  def parse_string(string)
+    length, content = string.split ':'
+    if content.length == length.to_i
+      content
+    else
+      raise InvalidEncodingError, "String length declared as #{length.to_i}, but was #{content.length} " 
     end
+  end
 
-    def parse_io_list(io)
-      list = []
-      until peek(io) == 'e' || io.eof?
-        case peek(io)
-        when 'i'
-          list << parse_int(io.gets sep='e')
-        when 'l'
-          list << parse_list(io)
-        when 'd'
-          list << parse_dict(io)
-        when ->(e) { e =~ /\d/ }
-          length = io.gets(sep=':').to_i
-          list << io.gets(length)
-        else
-          raise InvalidEncodingError, "Encountered unexpected identifier #{ peek io }"
-        end
-      end
-      io.getc
-      list
+  def make_hash_from_array(list)
+    hash = {}
+    list.each_slice(2) do |k,v|
+      hash[k] = v
     end
+    hash
+  end
+end
 
-    def make_hash_from_array(list)
-      hash = {}
-      list.each_slice(2) do |k,v|
-        hash[k] = v
-      end
-      hash
-    end
+class Encoder
+  attr_reader :object
 
-    def parse_int(string)
-      if string[0] == 'i' && string[-1] == 'e'
-        string[1..-2].to_i
-      else
-        raise InvalidEncodingError, 'Integer does not have closing e'
-      end
+  def self.encode(object)
+    new(object).encode
+  end
+  
+  def initialize(object)
+    @object = object
+  end
+  
+  def encode
+    case object
+    when Symbol
+      encode object.to_s
+    when String
+      encode_string object
+    when Integer
+      encode_int object
+    when Array
+      encode_array object
+    when Hash
+      encode_hash object
+    else
+      raise UnencodableTypeError, "Cannot encode instance of type #{object.class}"
     end
+  end
+  
+protected
 
-    def parse_string(string)
-      length, content = string.split ':'
-      if content.length == length.to_i
-        content
-      else
-        raise InvalidEncodingError, "String length declared as #{length.to_i}, but was #{content.length} " 
-      end
-    end
+  def encode_string(string)
+    "#{ string.length }:#{ string }"
+  end
 
-    def encode_string(string)
-      "#{ string.length }:#{ string }"
-    end
+  def encode_int(int)
+    "i#{ int }e"
+  end
 
-    def encode_int(int)
-      "i#{ int }e"
-    end
+  def encode_array(array)
+    array.inject("l") { |result, el| result += encode(el) } + "e"
+  end
 
-    def encode_array(array)
-      array.inject("l") { |result, el| result += encode(el) } + "e"
-    end
+  def encode_hash(hash)
+    hash.inject("d") { |result, (k,v)| result += "#{ encode(k.to_s) }#{ encode(v) }" } + 'e'
+  end
+end
 
-    def encode_hash(hash)
-      hash.inject("d") { |result, (k,v)| result += "#{ encode(k.to_s) }#{ encode(v) }" } + 'e'
-    end
+class BEncoder
+  def self.endcode(object)
+    Encoder.encode(object)
+  end
 
-    def peek(io)
-      char = io.getc
-      io.ungetc char
-      char
-    end
+  def self.decode(string)    
+    Decoder.parse(string)
   end
 end
